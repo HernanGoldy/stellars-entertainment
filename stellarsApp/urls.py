82cff3cca6eb0c32a8b1bec66f0279f14172976d
Patricio Pagano <patricio.pagano@paradigma.com.ar>
2022-10-14 09:56:40 -0300

    se agregan posts

--- stellarsApp/urls.py
+++ stellarsApp/urls.py
@@ -5,10 +5,10 @@ from stellarsApp import views
 
 
 urlpatterns = [
-    path('', views.home, name="Inicio"),
-    path('lounges', views.lounges, name="Salas"),
-    path('promotions', views.promotions, name="Promociones"),
-    path('previews', views.previews, name="Preestrenos"),
-    path('blog', views.blog, name="Blog"),
-    path('base', views.base, name="Base"),
+    path('/', views.home, name="Inicio"),
+    path('lounges/', views.lounges, name="Salas"),
+    path('promotions/', views.promotions, name="Promociones"),
+    path('previews/', views.previews, name="Preestrenos"),
+    path('blog/', views.blog, name="Blog"),
+    path('base/', views.base, name="Base"),
 ]
