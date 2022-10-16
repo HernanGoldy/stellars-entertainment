99c8d4875b8c82ddb1cceb4b8b3dfe130041fe18
mrojasls <marcelorojas@MacBook-Pro-5.fibertel.com.ar>
2022-10-16 10:14:37 -0300

    Agrego descripcion peliculas en el sitio

--- stellarsApp/views.py
+++ stellarsApp/views.py
@@ -10,8 +10,22 @@ def lounges(request):
 def promotions(request):
     return render(request, 'stellarsApp/promotions.html')
 
-def previews(request):
-    return render(request, 'stellarsApp/previews.html')
+def previews(request):    
+    film = {
+            'title':'Amsterdam',
+            'title2': 'Drama / Comedia / +13',
+            'description': 'Historia épica de un crimen romántico, acerca de tres íntimos amigos que se ven envueltos en una de las tramas secretas más escandalosas de la historia norteamericana, basada en hechos de ficción. Producida por Arnon Milchan, Matthew Budman, Anthony Katagas, David O. Russell y Christian Bale. Productores ejecutivos: Yariv Milchan, Michael Schaefer, Sam Hanson.',
+            'trailer':"https://www.youtube.com/embed/Xp42uIKRJ9c",
+            'original': 'Amsterdam',
+            'country': 'Estados Unidos',
+            'genre': 'Drama - Comedia',
+            'year': '2022',
+            'direction': 'David O. Russell',
+            'cast': 'Christian Bale, Margot Robbie, John David Washington, Chris Rock, Anya Taylor-Joy, Zoe Saldaña, Mike Myers, Michael Shannon, Timothy Olyphant, Andrea Riseborough, Taylor Swift, Matthias Schoenaerts, Alessandro Nivola, Rami Malek, Robert De Niro',
+            'duration': '134 min',
+            'distribution': 'Disney',
+    }
+    return render(request,'stellarsApp/previews.html',{'film': film})
 
 def blog(request):
     return render(request, 'stellarsApp/blog.html')
