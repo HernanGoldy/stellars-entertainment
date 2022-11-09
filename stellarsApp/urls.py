31e44e8d978b7e414875d1594da29aa8d16d87ce
HernanGoldy <hernanmdq@gmail.com>
2022-11-09 19:31:22 -0300

    Se agrega url de home

--- stellarsApp/urls.py
+++ stellarsApp/urls.py
@@ -5,11 +5,11 @@ from stellarsApp import views
 
 
 urlpatterns = [
-    path('', views.home, name="Inicio"),
+    path('home', views.home, name="Inicio"),
     path('lounges', views.lounges, name="Salas"),
     path('promotions', views.promotions, name="Promociones"),
     path('previews', views.previews, name="Preestrenos"),
     path('blog', views.blog, name="Blog"),
     path('base', views.base, name="Base"),
-    
+
 ]
